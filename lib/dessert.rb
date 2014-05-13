3c2af3d9d552ebcb0d0355ed32224429734c5db2
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-12 19:06:10 -0500

    Dessert OOP inheritance implementation

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,27 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(flavor)
-    # your code here
+      super "#{flavor} jelly bean", 5
+      @flavor = flavor
+  end
+
+  def delicious?
+      if @flavor == "licorice" then false
+      else super
+      end
   end
 end
