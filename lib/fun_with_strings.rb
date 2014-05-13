43bc8f4688926a8dd9561acf0d89900d3e3b229c
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-12 19:07:56 -0500

    Palindrome, WordCount and Anagrams Implementation

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,41 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    string = self.downcase.gsub(/(\W|\s)+/i,'')
+    string.reverse == string
   end
   def count_words
-    # your code here
+    dict = {}
+   if self != ""
+    self.downcase.gsub!(/\b\w*[^\W*]\b/){ |w|
+      p w
+      if dict.has_key? w
+        dict[w] += 1
+      else
+        dict[w] = 1
+      end
+    }
+   end
+    dict
   end
   def anagram_groups
-    # your code here
+    result = []
+    self.split(' ').each do |word|
+      added = false
+      if result.size > 0
+        result.each do |arr|
+          arr.each do |el|
+            if el.downcase.chars.sort == word.downcase.chars.sort
+              arr << word
+              added = true
+              break
+            end
+          end
+          break if added
+        end
+      end
+      result << [word] unless added
+    end
+    result
   end
 end
 
