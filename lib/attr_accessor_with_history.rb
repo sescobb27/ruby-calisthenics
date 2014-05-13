1401d4ec787c87f8f807b7e11f3f9aa9bb86bb43
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-12 19:06:38 -0500

    Metaprogramming Implementation

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,19 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(args)
+        @#{attr_name} = args
+        @#{attr_name}_array ||= [nil]
+        @#{attr_name}_array << args
+      end
+
+      def #{attr_name}_history
+        if @#{attr_name}_array.nil? || @#{attr_name}_array.length == 1
+          return @#{attr_name}_array
+        else
+          return @#{attr_name}_array[0..-2]
+        end
+      end
     }
   end
 end
